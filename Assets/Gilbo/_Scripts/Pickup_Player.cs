506ecf0263360f2750176eec24ed16fc7614a380
gilbo99 <53989937+gilbo99@users.noreply.github.com>
2023-09-05 13:23:17 +1000

    Added ANim

--- Assets/Gilbo/_Scripts/Pickup_Player.cs
+++ Assets/Gilbo/_Scripts/Pickup_Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEditor.PackageManager.UI;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Pickup_Player : MonoBehaviour
 {
@@ -11,7 +12,10 @@ public class Pickup_Player : MonoBehaviour
     public LayerMask layerMask;
     public float pickup_Range;
     public GameObject head;
-    public GameObject hand;
+    public GameObject M_Bat;
+    public GameObject M_FlashLight;
+    public GameObject M_GlassShard;
+    public GameObject M_Scalpel;
 
     private GameObject weapon;
 
@@ -22,7 +26,7 @@ public class Pickup_Player : MonoBehaviour
 
     private void Awake()
     {
-        m_Animator = hand.GetComponent<Animator>();
+        
     }
 
     void Update()
@@ -31,9 +35,25 @@ public class Pickup_Player : MonoBehaviour
 
 
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && weapon != null)
         {
-            m_Animator.SetTrigger("Player_Attack");
+            
+            if (weapon.CompareTag("Bat"))
+            {
+                M_Bat.GetComponent<Animator>().SetTrigger("Player_Attack");
+            }
+            if (weapon.CompareTag("FlashLight"))
+            {
+                M_FlashLight.GetComponent<Animator>().SetTrigger("Attack_Flashlight");
+            }
+            if (weapon.CompareTag("GlassShard"))
+            {
+                M_GlassShard.GetComponent<Animator>().SetTrigger("Attack_GlassShard");
+            }
+            if (weapon.CompareTag("Scalpel"))
+            {
+                M_Scalpel.GetComponent<Animator>().SetTrigger("Player_Attack");
+            }
         }
 
 
@@ -41,9 +61,34 @@ public class Pickup_Player : MonoBehaviour
 
         if (inHand == true && Input.GetKeyDown("e"))
         {
-            hand.transform.DetachChildren();
-            weapon.GetComponent<Rigidbody>().isKinematic = false;
-            inHand = false;
+
+            if (weapon.CompareTag("Bat"))
+            {
+                M_Bat.transform.DetachChildren();
+                weapon.GetComponent<Rigidbody>().isKinematic = false;
+                inHand = false;
+            }
+            if (weapon.CompareTag("FlashLight"))
+            {
+                M_FlashLight.transform.DetachChildren();
+                weapon.GetComponent<Rigidbody>().isKinematic = false;
+                inHand = false;
+
+            }
+            if (weapon.CompareTag("GlassShard"))
+            {
+                M_GlassShard.transform.DetachChildren();
+                weapon.GetComponent<Rigidbody>().isKinematic = false;
+                inHand = false;
+
+            }
+            if (weapon.CompareTag("Scalpel"))
+            {
+                M_Scalpel.transform.DetachChildren();
+                weapon.GetComponent<Rigidbody>().isKinematic = false;
+                inHand = false;
+
+            }
 
 
         }
@@ -57,24 +102,63 @@ public class Pickup_Player : MonoBehaviour
         // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(head.transform.position, head.transform.TransformDirection(Vector3.forward), out hit, pickup_Range, layerMask))
         {
-            Debug.DrawRay(head.transform.position, head.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            if(hit.collider.gameObject.CompareTag("Pickable") && Input.GetKeyDown("e") && inHand == false)
+            
+            if(Input.GetKeyDown("e") && inHand == false)
             {
                 weapon = hit.collider.gameObject;
+                if (hit.collider.gameObject.CompareTag("Bat"))
+                {
+                    weapon.transform.parent = M_Bat.transform;
+                    weapon.GetComponent<Rigidbody>().isKinematic = true;
+                    weapon.transform.rotation = new Quaternion(0, 0, 0, 0);
+                    weapon.transform.position = new Vector3(0, 0, 0);
+                    weapon.transform.position = M_Bat.transform.position;
+                   
+                }
+                
+                if (hit.collider.gameObject.CompareTag("FlashLight"))
+                {
+                    
+                    weapon.transform.parent = M_FlashLight.transform;
+                    weapon.GetComponent<Rigidbody>().isKinematic = true;
+                    weapon.transform.rotation = new Quaternion(0, 0, 0, 0);
+                    weapon.transform.position = new Vector3(0, 0, 0);
+                    weapon.transform.position = M_FlashLight.transform.position;
 
-                weapon.transform.parent = hand.transform;
-                weapon.GetComponent<Rigidbody>().isKinematic = true;
-                weapon.transform.rotation = new Quaternion(0, 0, 0, 0);
-                weapon.transform.position = new Vector3(0, 0, 0);
-                weapon.transform.position = hand.transform.position;
+                    
+                }
+
+                if (hit.collider.gameObject.CompareTag("GlassShard"))
+                {
+
+                    weapon.transform.parent = M_GlassShard.transform;
+                    weapon.GetComponent<Rigidbody>().isKinematic = true;
+                    weapon.transform.rotation = new Quaternion(0, 0, 0, 0);
+                    weapon.transform.position = new Vector3(0, 0, 0);
+                    weapon.transform.position = M_GlassShard.transform.position;
+
+                    
+                }
+
+                if (hit.collider.gameObject.CompareTag("Scalpel"))
+                {
+
+                    weapon.transform.parent = M_Scalpel.transform;
+                    weapon.GetComponent<Rigidbody>().isKinematic = true;
+                    weapon.transform.rotation = new Quaternion(0, 0, 0, 0);
+                    weapon.transform.position = new Vector3(0, 0, 0);
+                    weapon.transform.position = M_Scalpel.transform.position;
+
+                    
+                }
                 inHand = true;
-                
 
 
-                
 
             }
-            
+
+
+            Debug.DrawRay(head.transform.position, head.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
         }
         
 
