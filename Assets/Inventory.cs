81dd9cb5860827ce2c78f467a94e5458914a5943
gilbo99 <53989937+gilbo99@users.noreply.github.com>
2023-09-06 00:07:41 +1000

    Adding

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -7,6 +7,9 @@ public class Inventory : MonoBehaviour
     public int Pill;
     public int Drugs;
     public Light WorldLight;
+    public List<GameObject> spawnable;
+    public List<GameObject> weaponds;
+    public bool lights;
     public void PickUpItem(int ItemIndex)
     {
         switch(ItemIndex)
@@ -24,12 +27,27 @@ public class Inventory : MonoBehaviour
     {
         if(Pill >= 1)
         {
-            PillCollected();
+            if (!lights)
+            {
+                PillCollected();
+            }
         }
     }
 
     public void PillCollected()
     {
         WorldLight.intensity = 0;
+
+        for(int i = 0; i < spawnable.Count; i++)
+        {
+            spawnable[i].SetActive(true);
+        }
+
+        for(int i = 0; i < weaponds.Count; i++)
+        {
+            weaponds[i].SetActive(true);
+        }
+
+
     }
 }
